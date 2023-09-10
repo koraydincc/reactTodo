0c65040b6a09511ef9b8a2163a1e6f70eeb1fdc6
= <koraydincc@icloud.com>
2023-09-10 11:49:58 +0300

    Project
    Project

--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -1,10 +1,8 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { MyContext } from '../pages/HomePage';
 
 function Todo() {
   const { todos, setTodos } = useContext(MyContext);
-  
-
 
   const handleDelete = (id) => {
     const updatedTodos = todos.filter((todo) => todo.id !== id);
@@ -12,19 +10,30 @@ function Todo() {
   };
 
   return (
-    <div>
+    <>
       <h2 className='title'>Todo List</h2>
-      <ul>
-        {todos.map((todo) => (
-          <ul className='todo' key={todo.id}>
-            {todo.text}
-            <button>Düzenle</button>
-            <button onClick={() => handleDelete(todo.id)}>Sil</button>
-          </ul>
-        ))}
-      </ul>
-    
-    </div>
+      <div className='todo'>
+        <ul>
+          {todos.map((todo) => (
+            <ul className='todo-content' key={todo.id}>
+              <div className='todo-text'>
+                <h4>{todo.id})</h4>
+                {todo.text}
+              </div>
+              <div className='alan'>
+                    {todo.category}
+              </div>
+              <div className='todo-actions'>
+                {/* Düzenle düğmesini eklemek için */}
+                <button>Düzenle</button>
+                {/* Sil düğmesini düzgün hale getirmek için */}
+                <button onClick={() => handleDelete(todo.id)}>Sil</button>
+              </div>
+            </ul>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
--- todoapp/src/pages/HomePage.jsx
+++ todoapp/src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import Todo from '../components/Todo';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export const MyContext = createContext();
 
 function HomePage() {
   const [todos, setTodos] = useState([]);
   const [value, setValue] = useState('');
+  const [selectValue, setSelectValue]=useState('')
   const [idCounter, setIdCounter] = useState(1); // İlk id değeri 1 olarak başlasın
 
   const handleValue = () => {
@@ -14,6 +20,7 @@ function HomePage() {
       const newTodo = {
         id: idCounter, // id değeri idCounter ile atanır
         text: value,
+        category: selectValue
       };
 
       setTodos([...todos, newTodo]);
@@ -21,6 +28,17 @@ function HomePage() {
       setIdCounter(idCounter + 1); // idCounter'ı bir artırarak bir sonraki id'yi hazırla
     }
   };
+
+  const handleChange = (event) => {
+    const selectedValue = event.target.value
+    setSelectValue(selectedValue)
+
+   
+  }
+
+  useEffect(()=>{
+    console.log(selectValue)
+  },[selectValue])
 
   return (
     <div className='headerDiv'>
@@ -34,10 +52,26 @@ function HomePage() {
           <button onClick={handleValue}>
             <PostAddIcon />
           </button>
+          <FormControl>
+              <InputLabel id="demo-simple-select-label">Alan Seçimi</InputLabel>
+              <Select
+                className='selectBox'
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectValue}
+                label="Alan Seçimi"
+                onChange={handleChange}
+              >
+                <MenuItem value='Spor'>Spor</MenuItem>
+                <MenuItem value='Kişisel'>Kişisel</MenuItem>
+                <MenuItem value='İş'>İş</MenuItem>
+              </Select>
+          </FormControl>
+          
         </div>
       </div>
       <div className='mainTodo'>
-        <MyContext.Provider value={{ todos, setTodos,idCounter }}>
+        <MyContext.Provider value={{ todos, setTodos,idCounter, selectValue }}>
           <Todo />
         </MyContext.Provider>
       </div>
